be424cff95792e0451bdf6c03e473ab288e62df4
Alejandro Pacheco <alepacheco0827@gmail.com>
2024-05-14 20:11:43 -0400

    Initial commits configuring Vite, React, aand Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        AI-Driven Video Creation and Management Platform
+      </h1>
+    </div>
+  );
+};
+
+export default App;
